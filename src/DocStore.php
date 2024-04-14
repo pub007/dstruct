e2e9d71d9023336e96b1789668ed6085a0c0d6b6
David Lidstone <david@lidstone.me>
2024-04-14 22:27:18 +0100

    DocStore default changes

--- src/DocStore.php
+++ src/DocStore.php
@@ -6,8 +6,14 @@ class DocStore
 
 	protected static $stores = [];
 
-	public static function gi(string $collection = '', string $store = "cache")
+	public static function gi(string $collection = '', ?string $store)
 	{
+		$store = $store ?? $_ENV['STORE_DEFAULT'] ?? false;
+
+		if (! $store) {
+			throw new \ErrorException("Unable to find a Store");
+		}
+
 		if (! isset(self::$stores[$store])) {
 			$storesSettings = Prefs::gi()->get('stores');
 			self::$stores[$store] = new \MongoDB\Client($storesSettings[$store]['connectionString'], [], $storesSettings[$store]['options']);
